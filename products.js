a4507cdd8b9f0bc7ec4db723f3d9a582283ad8bb
devanmagicapps <devanmagicapps@gmail.com>
2025-09-27 01:23:08 +0800

    Update products.js

--- products.js
+++ products.js
@@ -4,7 +4,7 @@ export const apps = [
         name: "GeoSense",
         price: 75000,
         shortDescription: "Prediksi menggunakan Google Map ASLI",
-        fullDescription: "GeoSense adalah aplikasi prediksi free choice menggunakan aplikasi Google Map bawaan hp anda. Bayangkan : anda dari awal sudah mencari satu nama daerah di Google Map anda. Lalu anda menyuruh penonton untuk memilih satu dari sekian banyak nama daerah TANPA PAKSAAN! di akhir, prediksi anda akan selalu tepat 100%. <br>- TANPA PAKSAAN<br>- TANPA MENGGUNAKAN MIC<br>- 100% FREE CHOICE",
+        fullDescription: "GeoSense adalah aplikasi prediksi free choice menggunakan aplikasi Google Map bawaan hp anda. Bayangkan : anda dari awal sudah mencari satu nama daerah di Google Map anda. Lalu anda menyuruh audience untuk memilih satu dari sekian banyak nama daerah <strong>TANPA PAKSAAN!</strong> di akhir, prediksi anda akan selalu tepat 100%. <br>- <strong>TANPA PAKSAAN!</strong><br>- TANPA MENGGUNAKAN MIC<br>- <strong>100% FREE CHOICE</strong>",
         image: "https://placehold.co/400x300/1e1e1e/d1d5db?text=GEOSENSE",
         preview: "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1"
     },
@@ -13,7 +13,7 @@ export const apps = [
         name: "RPS Prediction",
         price: 100000, // Contoh harga, dapat disesuaikan
         shortDescription: "Prediksi Permainan Gunting Batu Kertas 100% benar!.",
-        fullDescription: "RPS Prediction adalah senjata Mentalist modern untuk memprediksi skor total (jumlah Menang, Kalah, dan Seri) dari permainan Gunting Batu Kertas (RPS) sebelum permainan dimulai. Penonton bermain melalui tautan QR Code di ponsel mereka, menjadikannya 100% *fair play* di mata mereka. Anda memiliki dua mode kontrol rahasia:\n<br>- **Magician Mode (Otomatis):** Sistem menyesuaikan pilihan Anda di detik terakhir untuk memastikan hasil sesuai target prediksi Anda.\n<br>- <strong>Peek Mode (Manual):</strong> Anda melihat pilihan audiens secara *real-time* untuk menentukan pilihan Anda sendiri.\n<br><br>Hasil skor akhir akan selalu cocok 100% dengan prediksi yang sudah Anda tulis di amplop tertutup sejak awal.",
+        fullDescription: "Bayangkan : Anda memberi tau audience bahwa Anda telah menulis prediksi sebelum permainan dimulai. prediksi ini bisa dipegang oleh oleh audience dari awal permainan. Lalu <strong>Audience</strong> mengeluarkan ponsel mereka, memindai QR, dan bermain melawan Anda dalam beberapa ronde. Setiap ronde, mereka memilih Gunting, Batu, atau Kertas secara bebas, dan hasilnya ditampilkan secara instan di ponsel mereka.<br><br>Mereka merasa permainan ini <strong>fair play 100%</strong>. Namun, saat pertandingan usai, prediksi anda) <strong>COCOK TEPAT 100%</strong> dengan tulisan di amplop prediksi yang sudah ada di meja sejak awal!<br><br>Ini adalah permainan Mentalism modern yang sangat kuat kuat: permainan bisa dilakukan <strong>TANPA FORCING</strong>, dan hasil akhir akan selalu tepat!.",
         image: "https://placehold.co/400x300/FEB47B/4A4A4A?text=RPS+PREDICTION",
         preview: "https://www.youtube.com/embed/placeholder_rps_trailer" // Harap ganti dengan tautan trailer yang sebenarnya
     }
